41c0af7d198b05b7cdf65aae62c12ae15613fe0c
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-01-15 06:19:52 +0700

    update android page

--- src/components/Android.jsx
+++ src/components/Android.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Grid, Cell} from 'react-mdl';
 import { NavLink, Button } from 'reactstrap';
+import kalkulator from './foto/kalkulator.png';
+import recycle from './foto/recycleview.png';
 
 import {Link} from 'react-router-dom';
 
@@ -11,27 +13,37 @@ class Apps extends Component {
                 <Grid className="beranda-grid">
                     <div className="karyaku">
 
-                        <div className="karya1">
+                        <div className="karya">
                             <img
-                                src="https://lh3.googleusercontent.com/isyLJsg-ybq8xsxkg2sIihjoztQPpXqUoESQi8saBP7SA-fPGFS2Em1LbmGknyxLnyvh=w720-h310-rw"></img>
+                                src={recycle}></img>
                             <h2 className="judul-portofolio">Recycle View Android</h2>
                             <p>
                                 
                             </p>
-                            <Button color="success" size="lg" block>Demo</Button>
-                            <Button variant="primary"style={{marginTop:'20px'}}><NavLink size="lg" block color="success"  href="https://play.google.com/store/apps/details?id=com.contoh.rachgunawan">Download</NavLink></Button>
+                            <Button color="secondary" block size="lg" style={{marginTop:'5px'}}>
+                                <NavLink className="buttonGithub" style={{margin:"0px"}} href="https://github.com/nawan44/Dicoding-Android-Pemula">
+                                    Github</NavLink></Button>
+                            <br ></br>
+                            <Button color="info" block size="lg" style={{marginTop:'5px'}}>
+                                <NavLink className="buttonPlaystore" style={{margin:"0px"}} href="https://play.google.com/store/apps/details?id=com.contoh.rachgunawan">
+                                    Playstore</NavLink></Button>
+
 
                         </div>
-                        <div className="karya1">
+                        <div className="karya">
                             <img
-                                src="https://lh3.googleusercontent.com/isyLJsg-ybq8xsxkg2sIihjoztQPpXqUoESQi8saBP7SA-fPGFS2Em1LbmGknyxLnyvh=w720-h310-rw"></img>
+                                src={kalkulator}></img>
                             <h2 className="judul-portofolio">Kalkulator Android</h2>
                             <p>
                                 
                             </p>
-                            <Button size="lg" style={{marginTop:'20px'}}><NavLink href="https://github.com/nawan44/Kalkulator-Android-Java/tree/developer">Github</NavLink></Button>
-                            <Button size="lg" style={{marginTop:'20px'}}><NavLink size="lg" block color="success"  href="https://play.google.com/store/apps/details?id=com.nawina.kalkulator">Download</NavLink></Button>
-
+                            <Button color="secondary" block size="lg" style={{marginTop:'5px'}}>
+                                <NavLink className="buttonGithub" style={{margin:"0px"}} href="https://github.com/nawan44/Kalkulator-Android-Java/tree/developer">
+                                    Github</NavLink></Button>
+                            <br ></br>
+                            <Button block color="info" size="lg" style={{marginTop:'5px'}}>
+                                <NavLink className="buttonPlaystore" style={{margin:"0px"}} href="https://play.google.com/store/apps/details?id=com.nawina.kalkulator">
+                                    Playstore</NavLink></Button>
                         </div>
                     </div>
 
