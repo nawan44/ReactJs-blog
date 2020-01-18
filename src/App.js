7fb6faaa8247a7265ba01ae497cf475a1991181c
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-01-19 06:43:45 +0700

    new style color

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         return (
             <div className="demo-big-content">
                 <Layout>
-                    <Header className="header-color" title="Portofolio" scroll>
+                    <Header className="header-color" title="Portofolio" style={{color:"#00ff00"}} scroll> 
                         <Navigation >
                             <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/Home" >Home</Link>
                             <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/AboutMe">About Me</Link>
@@ -19,13 +19,14 @@ class App extends Component {
                             <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/Apps">Android</Link>
                         </Navigation>
                     </Header>
-                    <Drawer title="Portfolio">
+                    <Drawer title="Portofolio" style={{background: "linear-gradient(to right, #2980B9, #6DD5FA)", color:"#00ff00" , fontSize:"25px" }}>
+                    
                         <Navigation >
-                            <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/Home">Home  </Link>
-                            <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/AboutMe">About Me </Link>
-                            <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/Game">Game</Link>
-                            <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/Apps">Android</Link>
-                            <Link style={{color:"green", fontStyle:"Bold", fontSize:"15px"}} to="/Web">Website</Link>
+                            <Link style={{color:"#00ff00", fontStyle:"Bold", fontSize:"15px"}} to="/Home">Home  </Link>
+                            <Link style={{color:"#00ff00", fontStyle:"Bold", fontSize:"15px"}} to="/AboutMe">About Me </Link>
+                            <Link style={{color:"#00ff00", fontStyle:"Bold", fontSize:"15px"}} to="/Game">Game</Link>
+                            <Link style={{color:"#00ff00", fontStyle:"Bold", fontSize:"15px"}} to="/Apps">Android</Link>
+                            <Link style={{color:"#00ff00", fontStyle:"Bold", fontSize:"15px"}} to="/Web">Website</Link>
                         </Navigation>
                     </Drawer>
                     <Content>
