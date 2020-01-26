4bfa464243e00f0f51c03c6dbbbb91297d41fdee
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-01-26 23:52:35 +0700

    new menu + change style

---
+++ src/components/DataScientist.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Grid} from 'react-mdl';
+import {Link} from 'react-router-dom';
+import {Button, NavLink} from 'reactstrap';
+class Game extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default DataScientist;
