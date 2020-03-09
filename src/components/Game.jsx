456a679544fd962f230be445dbb24b238649b76d
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-03-10 00:07:23 +0700

    menu game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import {Grid} from 'react-mdl';
 import {Link} from 'react-router-dom';
+import game2d from './foto/2d.png';
+import game3d from './foto/3d.png';
+import ar from './foto/ar.png';
+import vr from './foto/vr.png';
+
 import {Button, NavLink, Badge} from 'reactstrap';
 class Game extends Component {
     render() {
@@ -9,9 +14,7 @@ class Game extends Component {
                 <Grid className="beranda-grid">
                     <div className="karyaku">
                         <div className="karya">
-                            <iframe src="http://www.nawina2d.com"  frameBorder="none" width="100%" height="700px">
-                                Game
-                            </iframe>
+                        <img style={{height:"65%", width:"80%"}}  src={game2d}></img>
                             <h2 >Game 2D</h2>
                             <div className="lisk">
                                <h4>This game using the webgl platform, </h4> 
@@ -25,23 +28,25 @@ class Game extends Component {
                             <Button style={{ padding: "0px" }} block>
                                 <NavLink className="buttonGame" href="https://github.com/nawan44/Game-2D-Adventure" target="blank" rel="nofollow">Git</NavLink>
                             </Button>
+                            <Button style={{ padding: "0px" }} block >
+                                <NavLink className="buttonGame" href="https://nawina2d.com" target="blank" rel="nofollow">Demo</NavLink>
+                            </Button>
                         </div>
                          
                         <div className="karya">
-                            <iframe  src="http://www.nawina3d.com"  frameBorder="none" width="100%" height="700px">  Game
-                            </iframe>
+                        <img style={{height:"65%", width:"80%"}}  src={game3d}></img>
                             <h2 >Game 3D</h2>
                             <p></p>
                             <Button style={{ padding: "0px" }} block>
                                 <NavLink className="buttonGame" href="https://github.com/nawan44/Game-3D-Survival" target="blank" rel="nofollow">Git</NavLink>
                             </Button>
+                            <Button style={{ padding: "0px", color:"info" }} block  >
+                                <NavLink className="buttonGame" href="https://nawina3d.com" target="blank" rel="nofollow">Demo</NavLink>
+                            </Button>
                         </div>
                          
                         <div className="karya">
-                            <img
-                            style={{height:"65%", width:"80%"}}
-                                src="https://lh3.googleusercontent.com/PkBg6wYYdAR1PJ7FiqfSpqac3MTKA6ZskOaxrkrflYkFLSRPa18NssZgzeISDECV-mM=w720-h310-rw"></img>
-                            
+                        <img style={{height:"65%", width:"80%"}}  src={ar}></img>
                             <h2 style={{marginBottom:"20px"}} >Augmented Reality</h2>
                             <div className="lisk">
                                <h4>This Augmented Reality was built with </h4> 
@@ -62,28 +67,31 @@ class Game extends Component {
                             </Button>
 
                         </div>
-                         {/*
+                         
                         <div className="karya">
-                            <iframe
-                                src="http://www.nawinavr.com"
-                                frameBorder="none"
-                                width="100%"
-                                height="700px">
-                                Game
-                            </iframe>
-                            <h2 >Virtual Reality</h2>
+                            <img style={{height:"65%", width:"80%"}}  src={vr}></img>
+                            
+                            <h2 style={{marginBottom:"20px"}} >Virtual Reality</h2>
+                            <div className="lisk">
+                               <h4>This Virtual Reality was built with </h4> 
+                                    <p></p><h5>Unity Engine</h5>
+                                    <p></p><h5>Software Development Kit Oculus</h5>
+                                    <p></p><h5>Software Development Kit Google</h5>
+                                    <p></p><h5>C#</h5>
+                            </div>
                             <p></p>
-
                             <Button
                                 style={{
                                 padding: "0px"
                             }}
                                 block>
-                                <NavLink className="buttonGame">Git</NavLink>
+                                <NavLink
+                                    className="buttonGame"
+                                    href="https://github.com/nawan44/Virtual-Reality-00" target="blank" rel="nofollow">Git</NavLink>
                             </Button>
 
                         </div>
-                        */}
+                        
                     </div>
                 </Grid>
             </div>
