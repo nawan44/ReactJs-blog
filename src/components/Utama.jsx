e9c875cf3c65e5beec328c0ac4e52994d5b830db
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-03-27 22:21:30 +0700

    kill menu

--- src/components/Utama.jsx
+++ src/components/Utama.jsx
@@ -4,8 +4,6 @@ import Home from './Home';
 import AboutMe from './AboutMe';
 import Game from './Game';
 import Game2DAdventure from './Game/Game2D/Game2DAdventure'
-import Game3D from './Game3D';
-import GameAR from './GameAR';
 import Website from './Website';
 import Apps from './Android';
 
@@ -15,8 +13,6 @@ const Utama = () => (
         <Route path="/Home" component={Home}/>
         <Route path="/AboutMe" component={AboutMe}/>
         <Route path="/Game" component={Game}/>
-        <Route path="/Game3D" component={Game3D}/>
-        <Route path="/GameAR" component={GameAR}/>
         <Route path="Game2DAdventure" component={Game2DAdventure}/>
         <Route path="/Apps" component={Apps}/>
         <Route path="/Website" component={Website}/>
