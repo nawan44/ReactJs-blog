93b424ea0863cec644e5ed3324c3d40ee8e4e819
nawan44 <rachmat.d.gunawan@gmail.com>
2020-04-20 23:16:12 +0700

    my project

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -9,8 +9,39 @@ class Apps extends Component {
     render() {
         return (
             <div>
-                
-            </div>
+            <Grid className="beranda-grid">
+                <div className="karyaku">
+                    <div className="karya">
+                        <img src={recycle}></img>
+                        <h2>Recycle View Android</h2>
+                        <div className="lisk">
+                           <h4>This web project was built with </h4> 
+                                <p></p><h5>Java</h5>
+                        </div>
+                        <p></p>
+                        <Button color="secondary" block size="lg" style={{marginTop:'5px'}}>
+                            <NavLink className="buttonGithub" style={{margin:"0px"}} href="https://github.com/nawan44/Dicoding-Android-Pemula" target="blank" rel="nofollow">
+                                Github</NavLink></Button>
+                        <br ></br>
+                        <Button color="primary" block size="lg" style={{marginTop:'5px'}}>
+                            <NavLink className="buttonPlaystore" style={{margin:"0px"}} href="https://play.google.com/store/apps/details?id=com.contoh.rachgunawan" target="blank" rel="nofollow">
+                                Playstore</NavLink></Button>
+                    </div>
+                    <div className="karya">
+                        <img src={kalkulator}></img>
+                        <h2>Kalkulator Android</h2>
+                        <p> </p>
+                        <Button color="secondary" block size="lg" style={{marginTop:'5px'}}>
+                            <NavLink className="buttonGithub" style={{margin:"0px"}} href="https://github.com/nawan44/Kalkulator-Android-Java/tree/developer" target="blank" rel="nofollow">
+                                Github</NavLink></Button>
+                        <br ></br>
+                        <Button block color="primary" size="lg" style={{marginTop:'5px'}}>
+                            <NavLink className="buttonPlaystore" style={{margin:"0px"}} href="https://play.google.com/store/apps/details?id=com.nawina.kalkulator" target="blank" rel="nofollow">
+                                Playstore</NavLink></Button>
+                    </div>
+                </div>
+            </Grid>
+        </div>
         )
     }
 }
