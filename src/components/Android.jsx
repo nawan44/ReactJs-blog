2ab6319a0fcd998726ab304a94631c8c74ca8458
nawan44 <rachmat.d.gunawan@gmail.com>
2020-12-09 20:16:19 +0700

    android

--- src/components/Android.jsx
+++ src/components/Android.jsx
@@ -22,31 +22,39 @@ class Apps extends Component {
                         <h1>Portofolio Mobile App Android</h1>
                         <div className="container-porto">
                             <div className="porto">
-                                <img src={recycle}></img>
                                 <h2>Recycle View Android</h2>
-                                <div className="lisk">
-                                    <h4>This web project was built with </h4>
-                                    <p></p><h5>Java</h5>
+                                <img src={recycle} />
+                                <div className="porto-desc-container">
+                                    <div className="porto-desc">
+                                        <p>This web project was built with</p>
+                                        Java
+                                    </div>
                                 </div>
-                                <p></p>
-                                <Button color="secondary" block size="lg" style={{ marginTop: '5px' }}>
-                                    <NavLink className="buttonGithub" style={{ margin: "0px" }} href="https:github.com/nawan44/Dicoding-Android-Pemula" target="blank" rel="nofollow">
+
+                                <Button block="block" style={buttonPorto} >
+                                    <NavLink className="porto-button-git" href="https:github.com/nawan44/Dicoding-Android-Pemula" target="blank" rel="nofollow">
                                         Github</NavLink></Button>
                                 <br ></br>
-                                <Button color="primary" block size="lg" style={{ marginTop: '5px' }}>
-                                    <NavLink className="buttonPlaystore" style={{ margin: "0px" }} href="https:play.google.com/store/apps/details?id=com.contoh.rachgunawan" target="blank" rel="nofollow">
+                                <Button block="block" style={buttonPorto} >
+                                    <NavLink className="porto-button-demo" href="https:play.google.com/store/apps/details?id=com.contoh.rachgunawan" target="blank" rel="nofollow">
                                         Playstore</NavLink></Button>
                             </div>
                             <div className="porto">
-                                <img src={kalkulator}></img>
                                 <h2>Kalkulator Android</h2>
-                                <p> </p>
-                                <Button color="secondary" block size="lg" style={{ marginTop: '5px' }}>
-                                    <NavLink className="buttonGithub" style={{ margin: "0px" }} href="https:github.com/nawan44/Kalkulator-Android-Java/tree/developer" target="blank" rel="nofollow">
+
+                                <img src={kalkulator} />
+                                <div className="porto-desc-container">
+                                    <div className="porto-desc">
+                                        <p>This web project was built with</p>
+                                        Java
+                                    </div>
+                                </div>
+                                <Button block="block" style={buttonPorto}>
+                                    <NavLink className="porto-button-git" href="https:github.com/nawan44/Kalkulator-Android-Java/tree/developer" target="blank" rel="nofollow">
                                         Github</NavLink></Button>
                                 <br ></br>
-                                <Button block color="primary" size="lg" style={{ marginTop: '5px' }}>
-                                    <NavLink className="buttonPlaystore" style={{ margin: "0px" }} href="https:play.google.com/store/apps/details?id=com.nawina.kalkulator" target="blank" rel="nofollow">
+                                <Button block="block" style={buttonPorto}>
+                                    <NavLink className="porto-button-demo" href="https:play.google.com/store/apps/details?id=com.nawina.kalkulator" target="blank" rel="nofollow">
                                         Playstore</NavLink></Button>
                             </div>
                         </div>
